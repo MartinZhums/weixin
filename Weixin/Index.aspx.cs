974e9e183a401e880764ffdcada16ab3c7116418
Zhums <342650160@qq.com>
2017-06-12 10:43:05 +0800

    weixin

--- Weixin/Index.aspx.cs
+++ Weixin/Index.aspx.cs
@@ -11,7 +11,20 @@ namespace Weixin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Response.Write("1354");
+            string token = "weixin";
+            string timestamp = Request["timestamp"].ToString();
+            string nonce = Request["nonce"].ToString();
+            string signature = Request["signature"].ToString();
+            OperatWeixin ow = new OperatWeixin();
+
+            if (ow.checkSignature(token, timestamp, nonce, signature))
+            {
+                Response.Write("微信接入成功");
+            }
+            else
+            {
+                Response.Write("微信接入失败");
+            }
         }
     }
 }
--- Weixin/OperatWeixin.cs
+++ Weixin/OperatWeixin.cs
@@ -1,15 +1,42 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 
 namespace Weixin
 {
     public class OperatWeixin
     {
-        public bool CheckLogin()
+        public bool checkSignature(string token, string timestamp, string nonce, string signature)
         {
-            return true;
+            string[] a = { token, timestamp, nonce };
+            List<string> list = new List<string>();
+            list.Add(token);
+            list.Add(timestamp);
+            list.Add(nonce);
+            list.Sort();
+            string s = string.Join(",",list.ToArray());
+            SHA1 sha1 = new SHA1CryptoServiceProvider();
+            var buffer = Encoding.UTF8.GetBytes(s);
+            var data = SHA1.Create().ComputeHash(buffer);
+            StringBuilder sb = new StringBuilder();
+            foreach (var r in data)
+            {
+                sb.Append(r.ToString("X2"));
+            }
+            if (sb.ToString() == signature)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            
         }
     }
 }
