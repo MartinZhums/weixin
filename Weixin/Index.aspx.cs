cc1fa3bbf8c5232155f463791c9d729af80f34de
Zhums <342650160@qq.com>
2017-06-12 13:18:47 +0800

    weixin

--- Weixin/Index.aspx.cs
+++ Weixin/Index.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -11,22 +13,28 @@ namespace Weixin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string echoString = Request["echoStr"].ToString();
-            string token = "weixin";
-            string timestamp = Request["timestamp"].ToString();
-            string nonce = Request["nonce"].ToString();
-            string signature = Request["signature"].ToString();
+            //string echoString = Request["echoStr"].ToString();
+            //string token = "weixin";
+            //string timestamp = Request["timestamp"].ToString();
+            //string nonce = Request["nonce"].ToString();
+            //string signature = Request["signature"].ToString();
             OperatWeixin ow = new OperatWeixin();
 
-            if (ow.checkSignature(token, timestamp, nonce, signature))
-            {
-                Response.Write(echoString);
-                Response.End();  
-            }
-            else
-            {
-                Response.Write("微信接入失败");
-            }
+            //if (ow.checkSignature(token, timestamp, nonce, signature))
+            //{
+            //    Response.Write(echoString);
+            //    Response.End();  
+            //}
+            //else
+            //{
+            //    Response.Write("微信接入失败");
+            //}
+            FileStream fs1 = new FileStream(Server.MapPath(".") + "\\menu.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs1, Encoding.GetEncoding("GBK"));
+            string menu = sr.ReadToEnd();
+            sr.Close();
+            fs1.Close();
+            ow.HttpGet("https://api.weixin.qq.com/cgi-bin/menu/create?access_token=access_token", menu);  
         }
     }
 }
--- Weixin/OperatWeixin.cs
+++ Weixin/OperatWeixin.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 using System.Web;
@@ -60,6 +62,78 @@ namespace Weixin
                 return false;
             }
 
+        }
+        public string HttpGet(string uri, string postData)
+        {
+            Stream outstream = null;
+            Stream instream = null;
+            StreamReader sr = null;
+            HttpWebResponse response = null;
+            HttpWebRequest request = null;
+            Encoding encoding = Encoding.UTF8;
+            byte[] data = encoding.GetBytes(postData);
+            // 准备请求...  
+            try
+            {
+                // 设置参数  
+                request = WebRequest.Create(uri) as HttpWebRequest;
+                CookieContainer cookieContainer = new CookieContainer();
+                request.CookieContainer = cookieContainer;
+                request.AllowAutoRedirect = true;
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = data.Length;
+                outstream = request.GetRequestStream();
+                outstream.Write(data, 0, data.Length);
+                outstream.Close();
+                //发送请求并获取相应回应数据  
+                response = request.GetResponse() as HttpWebResponse;
+                //直到request.GetResponse()程序才开始向目标网页发送Post请求  
+                instream = response.GetResponseStream();
+                sr = new StreamReader(instream, encoding);
+                //返回结果网页（html）代码  
+                string content = sr.ReadToEnd();
+                string err = string.Empty;
+                return content;
+            }
+            catch (Exception ex)
+            {
+                string err = ex.Message;
+                return string.Empty;
+            }  
+        }
+        public string HttpPOST(string uri)
+        {
+            Stream instream = null;
+            StreamReader sr = null;
+            HttpWebResponse response = null;
+            HttpWebRequest request = null;
+            Encoding encoding = Encoding.UTF8;
+            // 准备请求...  
+            try
+            {
+                // 设置参数  
+                request = WebRequest.Create(uri) as HttpWebRequest;
+                CookieContainer cookieContainer = new CookieContainer();
+                request.CookieContainer = cookieContainer;
+                request.AllowAutoRedirect = true;
+                request.Method = "GET";
+                request.ContentType = "application/x-www-form-urlencoded";
+                //发送请求并获取相应回应数据  
+                response = request.GetResponse() as HttpWebResponse;
+                //直到request.GetResponse()程序才开始向目标网页发送Post请求  
+                instream = response.GetResponseStream();
+                sr = new StreamReader(instream, encoding);
+                //返回结果网页（html）代码  
+                string content = sr.ReadToEnd();
+                string err = string.Empty;
+                return content;
+            }
+            catch (Exception ex)
+            {
+                string err = ex.Message;
+                return string.Empty;
+            }  
         }
     }
 }
