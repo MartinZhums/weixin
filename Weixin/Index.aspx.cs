8ee53f9e47571114a09f950a87141da26908fa09
Zhums <342650160@qq.com>
2017-06-12 11:42:25 +0800

    weixin

--- Weixin/Index.aspx.cs
+++ Weixin/Index.aspx.cs
@@ -11,20 +11,26 @@ namespace Weixin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string echoString = HttpContext.Current.Request.QueryString["echoStr"];
             string token = "weixin";
             string timestamp = Request["timestamp"].ToString();
             string nonce = Request["nonce"].ToString();
             string signature = Request["signature"].ToString();
-            OperatWeixin ow = new OperatWeixin();
+            if (!string.IsNullOrEmpty(echoString))
+            {
+                HttpContext.Current.Response.Write(echoString);
+                HttpContext.Current.Response.End();
+            }  
+            //OperatWeixin ow = new OperatWeixin();
 
-            if (ow.checkSignature(token, timestamp, nonce, ref signature))
-            {
-                Response.Write("微信接入失败");
-            }
-            else
-            {
-                Response.Write("微信接入失败");
-            }
+            //if (ow.checkSignature(token, timestamp, nonce, ref signature))
+            //{
+            //    Response.Write("微信接入失败");
+            //}
+            //else
+            //{
+            //    Response.Write("微信接入失败");
+            //}
         }
     }
 }
