ad4bdcbae6a6f270a9e049767d55019dc12491e1
Zhums <342650160@qq.com>
2017-06-12 09:54:04 +0800

    weixin
    weixin

---
+++ Weixin/OperatWeixin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Weixin
+{
+    public class OperatWeixin
+    {
+        public bool CheckLogin()
+        {
+            return true;
+        }
+    }
+}
